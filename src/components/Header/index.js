27abb9326c524b2bdc899faa3c5d97a3ffe80941
Higor Zica <higorbhe@gmail.com>
2023-01-10 15:35:37 -0300

    Criação da estrutura de componenetes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,25 @@
+import Logosvg from '../../svg/laptop.svg';
+
 function Header() {
+    const descLogo = "Logo do Blog";
     return(
-        <h1>Header.js</h1>
+        <>
+        <header className='flex-space-between'>
+            <div className='logo border'>
+                <img src={Logosvg} alt={descLogo}/>
+            </div>
+            <div className='search'>
+                <input className='input-search' type='text' name='search' />
+            </div>
+            <ul className='menu'>
+                <li><a href='#' className='nav-link'> Categorias </a></li>
+                <li><a href='#' className='nav-link'> Sobre </a></li>
+                <li><a href='#' className='nav-link'> Contato </a></li>
+            </ul>
+
+        </header>
+        </>
+        
     );
 }
 
